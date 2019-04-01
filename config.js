1abe5af20eb625e64a0fedc19df862a796e98023
fender101 <40374424+fender101@users.noreply.github.com>
2019-04-01 12:17:47 -0700

    update

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // Mapbox API key
-const API_KEY = "use_your_key";
+const API_KEY = "pk.eyJ1IjoiZmVuZGVyMTAxIiwiYSI6ImNqcnhzbHBsMjBxeG80M3VrY2pyN2x2ZTIifQ.qh6jEOv7_wcwUpfl_boAaw";
 
 // Aeris key and info
 
-const apiId = "use_your_key";
+const apiId = "XPvRjjJwk7jY8JHijPi0L";
 
-const apiSecret = "use_your_key";
+// const apiSecret = "use_your_key";
 
 
 // aeris.config.set({
